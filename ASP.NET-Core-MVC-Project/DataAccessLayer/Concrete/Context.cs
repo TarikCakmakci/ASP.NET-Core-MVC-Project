7f1f76ac6b0774431ad4037a197e2aafb58744eb
onur sönmez <onursnmz@outlook.com>
2022-03-27 20:51:15 +0300

    Update
    About class oluşturuldu.
    DataAccess Layer katmanında context sınıfında sunucu database bağlantısı yapıldı.

---
+++ ASP.NET-Core-MVC-Project/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=94.73.147.7;database=u7174244_ProBudy; integrated security=true;");
+        }
+
+    }
+}
--- ASP.NET-Core-MVC-Project/EntityLayer/Concrete/About.cs
+++ ASP.NET-Core-MVC-Project/EntityLayer/Concrete/About.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,15 @@ namespace EntityLayer.Concrete
 {
     public class About
     {
-        //selamlar hocam
-        //bugün pazar parrar
+        [Key]
+
+        public int AboutId { get; set; }
+        public string AboutDetails1 { get; set; }
+        public string AboutDetails2 { get; set; }
+        public string AboutImage1 { get; set; }
+        public string AboutImage2 { get; set; }
+        public string AboutMapLocation { get; set; }
+        public string AboutStatus { get; set; }
+
     }
 }
