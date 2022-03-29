0a3f8d405203128a6c6fb8c1d0c4ee8723761f84
onur sönmez <onursnmz@outlook.com>
2022-03-29 22:48:25 +0300

    Update Interfaces and Repositories
    created interfaces and repositories

---
+++ ASP.NET-Core-MVC-Project/DataAccessLayer/Abstract/IPostDal.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IPostDal
+    {
+        List<Post> ListAllPost();
+        void AddPost(Post post);
+        void DeletePost(Post post);
+        void UpdatePost(Post post);
+        Post GetById(int id);
+    }
+}
