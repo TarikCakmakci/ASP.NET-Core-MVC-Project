5d26e7250634971baf13455c16eb44053324293c
onur sönmez <onursnmz@outlook.com>
2022-05-30 23:04:09 +0300

    Update UserComment

---
+++ ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Models/UserComment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_MVC_Project.Models
+{
+    public class UserComment
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+    }
+}
---
+++ ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/ViewComponents/CommentList.cs
@@ -0,0 +1,36 @@
+﻿using ASP.NET_Core_MVC_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_MVC_Project.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID=1,
+                    Username="Onur"
+                },
+                 new UserComment
+                {
+                    ID=2,
+                    Username="Tarık"
+                },
+                  new UserComment
+                {
+                    ID=3,
+                    Username="Mahir"
+                }
+
+            };
+            return View(commentvalues);
+        }
+    }
+}
