976a5a00c71d22a2834740e045df61fcbd0b91d5
onur sönmez <onursnmz@outlook.com>
2022-05-31 20:06:46 +0300

    Register İşlemi - Writer
    Register İşlemleri
    httpget
    httppost

---
+++ ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_MVC_Project.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Index(Writer p)
+        {
+            return View();
+        }
+    }
+}
