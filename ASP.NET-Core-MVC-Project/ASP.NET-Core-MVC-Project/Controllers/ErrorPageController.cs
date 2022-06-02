2873e4c3b8cd8da7078838393786eea346f39cfc
onur sönmez <onursnmz@outlook.com>
2022-06-02 20:13:33 +0300

    404 Page and Status Code
    404 sayfasına yönlendirme

---
+++ ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Controllers/ErrorPageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_MVC_Project.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        public IActionResult Error1(int code)
+        {
+            return View();
+        }
+    }
+}
--- ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Startup.cs
+++ ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Startup.cs
@@ -47,6 +47,9 @@ namespace ASP.NET_Core_MVC_Project
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1","?code={0}");
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
