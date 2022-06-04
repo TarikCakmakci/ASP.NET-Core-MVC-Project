04e5ef55a78136c98dbf43f1cf73114662647ad7
onur sönmez <onursnmz@outlook.com>
2022-06-05 00:07:41 +0300

    Authorize ile Return Login Url
    AllowAnonymus

--- ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Controllers/LoginController.cs
+++ ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Controllers/LoginController.cs
@@ -4,6 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EntityLayer.Concrete;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Http;
 
 namespace ASP.NET_Core_MVC_Project.Controllers
 {
@@ -13,6 +16,23 @@ namespace ASP.NET_Core_MVC_Project.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Index(Writer p)
+        {
+            Context c = new Context();
+            var datavalue = c.Writers.FirstOrDefault(x=> x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
+            if (datavalue != null)
+            {
+                HttpContext.Session.SetString("username",p.WriterMail);
+                return RedirectToAction("Index","Writer");
+            }
+            else
+            {
+                return View();
+            }
+            
         }
         
     }
