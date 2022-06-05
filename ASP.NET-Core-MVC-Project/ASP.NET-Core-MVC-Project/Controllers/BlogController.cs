de2d7ac975d3227052a9889b189392854850e300
Tarık Çakmakcı <tarikcakmakci@outlook.com>
2022-06-05 17:53:37 +0300

    Update
    Added new blog adding process in the author panel.
    Categories dropdown updated.

--- ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Controllers/BlogController.cs
+++ ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Controllers/BlogController.cs
@@ -1,7 +1,11 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +36,47 @@ namespace ASP.NET_Core_MVC_Project.Controllers
         {
             var values = bm.GetBlogListByWriter(1);
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult BlogAdd()
+        {
+            CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+            List<SelectListItem> categoryValues = (from x in cm.GetList()
+                select new SelectListItem
+                {
+                    Text=x.CategoryName,
+                    Value= x.CategoryId.ToString()
+
+                }).ToList();
+            ViewBag.cv = categoryValues;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult BlogAdd(Blog p)
+        {
+            BlogValidator bv = new BlogValidator();
+            ValidationResult results = bv.Validate(p);
+            if (results.IsValid)
+            {
+                p.BlogStatus = true;
+                //Status false yapılıp, post admin onayına sunulabilir.
+                p.BlogCreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                p.WriterId = 1;
+                bm.TAdd(p);
+                return RedirectToAction("BlogListByWriter", "Blog");
+
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+
+                }
+            }
+            return View();
+            
         }
     }
 }
---
+++ ASP.NET-Core-MVC-Project/BusinessLayer/ValidationRules/BlogValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class BlogValidator : AbstractValidator<Blog>
+    {
+        public BlogValidator()
+        {
+            RuleFor(x => x.BlogTitle).NotEmpty().WithMessage("Blog başlığını boş geçemezsiniz!");
+            RuleFor(x => x.BlogContent).NotEmpty().WithMessage("Blog içeriğini boş bırakamazsınız!");
+            RuleFor(x => x.BlogImage).NotEmpty().WithMessage("Blog görselinizi boş geçemezsiniz.");
+            RuleFor(x => x.BlogTitle).MaximumLength(150).WithMessage("Lütfen 150 karakterden daha az başlık giriniz.");
+            RuleFor(x => x.BlogTitle).MinimumLength(5).WithMessage("Lütfen 4 karakterden daha fazla başlık girişi yapınız.");
+
+        }
+    }
+}
