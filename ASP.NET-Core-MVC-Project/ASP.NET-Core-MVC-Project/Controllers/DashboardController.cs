549ac3e9b23120592c62d1a2f326258f5519c9de
Tarık Çakmakcı <tarikcakmakci@outlook.com>
2022-06-06 20:02:31 +0300

    Update
    Updated writer panel's dashboard

--- ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Controllers/DashboardController.cs
+++ ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,10 @@ namespace ASP.NET_Core_MVC_Project.Controllers
 
         public IActionResult Index()
         {
+            Context c = new Context();
+            ViewBag.v1 = c.Blogs.Count().ToString();
+            ViewBag.v2 = c.Blogs.Where(x => x.WriterId == 1).Count();
+            ViewBag.v3 = c.Categories.Count();
             return View();
         }
     }
