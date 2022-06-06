49ff2e72c8f40c80ac26d95fb681eb5c6edb1d28
Tarık Çakmakcı <tarikcakmakci@outlook.com>
2022-06-06 23:07:59 +0300

    Update
    Notifications page updated.

--- ASP.NET-Core-MVC-Project/DataAccessLayer/Migrations/20220606200231_mig_notificationColor_add.Designer.cs
+++ ASP.NET-Core-MVC-Project/DataAccessLayer/Migrations/20220606200231_mig_notificationColor_add.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20220606200231_mig_notificationColor_add")]
+    partial class mig_notificationColor_add
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ASP.NET-Core-MVC-Project/EntityLayer/Concrete/Notification.cs
+++ ASP.NET-Core-MVC-Project/EntityLayer/Concrete/Notification.cs
@@ -19,6 +19,8 @@ namespace EntityLayer.Concrete
         public DateTime NotificationDate{ get; set; }
 
         public bool NotificationStatus { get; set; }
+
+        public string NotificationColor { get; set; }
         
     }
 }
