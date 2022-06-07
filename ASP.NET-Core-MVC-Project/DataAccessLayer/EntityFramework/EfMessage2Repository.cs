8f5b4d0d3983db87b70a7feffd8883a9972a26ca
Tarık Çakmakcı <tarikcakmakci@outlook.com>
2022-06-07 23:45:58 +0300

    Update
    Messages section has been updated.

--- ASP.NET-Core-MVC-Project/DataAccessLayer/EntityFramework/EfMessage2Repository.cs
+++ ASP.NET-Core-MVC-Project/DataAccessLayer/EntityFramework/EfMessage2Repository.cs
@@ -1,6 +1,8 @@
 ﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.Repositories;
 using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,17 @@ namespace DataAccessLayer.EntityFramework
 {
     public class EfMessage2Repository : GenericRepository<Message2>, IMessage2Dal
     {
+        public List<Message2> GetListWithMessageByWriter(int id)
+        {
+            using (var c = new Context())
+            {
+
+                return c.Message2s.Include(x => x.SenderUser).Where(x => x.ReceiverID == id).ToList();
+                //Blog içerisine category sınıfından veri alacağımız için include metodu kullanıyoruz.
+
+            }
+        }
+
+      
     }
 }
