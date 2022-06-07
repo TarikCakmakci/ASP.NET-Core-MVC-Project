c911e5361856b556957230b88c7b29b800a6f3d8
Tarık Çakmakcı <tarikcakmakci@outlook.com>
2022-06-07 21:05:01 +0300

    Update
    Notification listing page updated.

--- ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/ViewComponents/Writer/WriterMessageNotification.cs
+++ ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/ViewComponents/Writer/WriterMessageNotification.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +10,13 @@ namespace ASP.NET_Core_MVC_Project.ViewComponents.Writer
 {
     public class WriterMessageNotification : ViewComponent
     {
+        MessageManager mm = new MessageManager(new EfMessageRepository());
         public IViewComponentResult Invoke()
         {
-            
-            return View();
+            string p;
+            p = "test@gmail.com";
+            var values = mm.GetInboxListByWriter(p);
+            return View(values);
         }
     }
 }
