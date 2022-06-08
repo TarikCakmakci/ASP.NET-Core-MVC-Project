8e80bd0594739dc900bf295de0647a478c01cc77
onur sönmez <onursnmz@outlook.com>
2022-06-08 19:31:06 +0300

    Excel Dynamic Table Export

---
+++ ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Areas/Admin/Models/BlogModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_MVC_Project.Areas.Admin.Models
+{
+    public class BlogModel
+    {
+        public int ID { get; set; }
+        public string BlogName { get; set; }
+    }
+}
