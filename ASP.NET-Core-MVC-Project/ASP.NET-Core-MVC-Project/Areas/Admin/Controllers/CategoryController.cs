4730c6e5ff83ed37e90a5ccfbed57ce962ed6699
Tarık Çakmakcı <tarikcakmakci@outlook.com>
2022-06-08 16:19:39 +0300

    Update
    Added a new category adding process from the admin panel.

--- ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Areas/Admin/Controllers/CategoryController.cs
+++ ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -19,6 +22,34 @@ namespace ASP.NET_Core_MVC_Project.Areas.Admin.Controllers
         {
             var values = cm.GetList().ToPagedList(page, 3);
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+            
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult results = cv.Validate(p);
+            if (results.IsValid)
+            {
+                p.CategoryStatus = true;
+                //Status false yapılıp, post admin onayına sunulabilir.
+                cm.TAdd(p);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+
+                }
+            }
+            return View();
         }
     }
 }
