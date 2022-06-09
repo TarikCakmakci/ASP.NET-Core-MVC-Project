fa946350f57d423934111b8f3be07941b10502de
onur sönmez <onursnmz@outlook.com>
2022-06-09 13:40:30 +0300

    Admin Table - Statistics Views

--- ASP.NET-Core-MVC-Project/DataAccessLayer/Concrete/Context.cs
+++ ASP.NET-Core-MVC-Project/DataAccessLayer/Concrete/Context.cs
@@ -73,8 +73,12 @@ namespace DataAccessLayer.Concrete
         public DbSet<BlogRayting> BlogRaytings { get; set; }
 
         public DbSet<Notification> Notifications { get; set; }
+
         public DbSet<Message> Messages { get; set; }
+
         public DbSet<Message2> Message2s { get; set; }
+
+        public DbSet<Admin> Admins { get; set; }
 
 
 
