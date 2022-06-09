de8adc4587b9295cf5ce294fb0201b58ba182d4b
onur sönmez <onursnmz@outlook.com>
2022-06-09 18:34:10 +0300

    Weather API

--- ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ASP.NET_Core_MVC_Project.Areas.Admin.ViewComponents.Statistic
 {
@@ -18,6 +19,11 @@ namespace ASP.NET_Core_MVC_Project.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.GetList().Count();
             ViewBag.v2 = c.Contacts.Count();
             ViewBag.v3 = c.Comments.Count();
+
+            string api = "27850a10c61fd78321d109229a11ed44";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api ;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
