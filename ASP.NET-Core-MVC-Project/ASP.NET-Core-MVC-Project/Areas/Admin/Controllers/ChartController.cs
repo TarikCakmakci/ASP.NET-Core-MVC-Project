e858b1457160eab41b8a98825b9caec8962def86
Tarık Çakmakcı <tarikcakmakci@outlook.com>
2022-06-09 18:51:46 +0300

    Update
    Google charts added

---
+++ ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,40 @@
+﻿using ASP.NET_Core_MVC_Project.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_MVC_Project.Areas.Admin.Controllers
+{
+    public class ChartController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+
+            list.Add(new CategoryClass
+            {
+                categoryName = "Teknoloji",
+                categoryCount = 10
+            });
+            list.Add(new CategoryClass
+            {
+                categoryName = "Yazılım",
+                categoryCount = 14
+            });
+            list.Add(new CategoryClass
+            {
+                categoryName = "Spor",
+                categoryCount = 5
+            });
+            return Json(new {jsonlist=list });
+        }
+    }
+}
---
+++ ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Areas/Admin/Models/CategoryClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_MVC_Project.Areas.Admin.Models
+{
+    public class CategoryClass
+    {
+        public int categoryCount { get; set; }
+
+        public string categoryName { get; set; }
+    }
+}
