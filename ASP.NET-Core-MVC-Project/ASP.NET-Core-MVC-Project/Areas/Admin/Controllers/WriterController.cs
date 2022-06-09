f423339398cc274a27259b0d8b7084e3001a132f
Tarık Çakmakcı <tarikcakmakci@outlook.com>
2022-06-09 20:14:55 +0300

    Update
    Update,delete,insert added with ajax

--- ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Areas/Admin/Controllers/WriterController.cs
+++ ASP.NET-Core-MVC-Project/ASP.NET-Core-MVC-Project/Areas/Admin/Controllers/WriterController.cs
@@ -20,7 +20,18 @@ namespace ASP.NET_Core_MVC_Project.Areas.Admin.Controllers
             var jsonWriters = JsonConvert.SerializeObject(writers);
             return Json(jsonWriters);
         }
-
+        public IActionResult GetWriterById(int writerid)
+        {
+            var findWriter = writers.FirstOrDefault(w => w.Id == writerid);
+            var jsonWriter = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriter);
+        }
+        public IActionResult GetById(int id)
+        {
+            var findWriter = writers.FirstOrDefault(w => w.Id == id);
+            var jsonWriter = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriter);
+        }
         public static List<WriterClass> writers = new List<WriterClass>
         {
             new WriterClass
@@ -39,5 +50,26 @@ namespace ASP.NET_Core_MVC_Project.Areas.Admin.Controllers
                 Name= "Onur"
             }
         };
+        [HttpPost]
+        public IActionResult AddWriter(WriterClass w)
+        {
+            writers.Add(w);
+            var jsonWriters = JsonConvert.SerializeObject(w);
+            return Json(jsonWriters);
+
+        }
+        public IActionResult DeleteWriter(int id)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == id);
+            writers.Remove(writer);
+            return Json(writer);
+        }
+        public IActionResult UpdateWriter(WriterClass w)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == w.Id);
+            writer.Name = w.Name;
+            var jsonWriter = JsonConvert.SerializeObject(w);
+            return Json(jsonWriter);
+        }
     }
 }
