9b80bf7622d7b8eab8a05c74c4ed2f2a0e0fb4f3
Tarık Çakmakcı <tarikcakmakci@outlook.com>
2022-06-09 21:03:55 +0300

    Update
    Created api project in main project.

---
+++ ASP.NET-Core-MVC-Project/BlogApi/DataAccessLayer/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApi.DataAccessLayer
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=78.135.105.238;Database=u7174244_ProBudy;User Id=u7174244_ProBudy; Password=KYyp18Q1HPaj97N;");
+        }
+    }
+}
