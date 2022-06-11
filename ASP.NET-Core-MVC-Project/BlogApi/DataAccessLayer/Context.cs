616fc585dd4ce189f8f41abfbaaca1d6ba49705d
onur sönmez <onursnmz@outlook.com>
2022-06-11 22:31:18 +0300

    Update
    Updated context.cs

--- ASP.NET-Core-MVC-Project/BlogApi/DataAccessLayer/Context.cs
+++ ASP.NET-Core-MVC-Project/BlogApi/DataAccessLayer/Context.cs
@@ -8,10 +8,7 @@ namespace BlogApi.DataAccessLayer
 {
     public class Context : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Server=78.135.105.238;Database=u7174244_Pro;User Id=u7174244_Pro; Password=AE7A70C1-13DB-4D9E-9C1E-07D0856700DE;");
-        }
+        
         public DbSet <Employee> Employees { get; set; }
     }
 }
--- ASP.NET-Core-MVC-Project/DataAccessLayer/Concrete/Context.cs
+++ ASP.NET-Core-MVC-Project/DataAccessLayer/Concrete/Context.cs
@@ -14,10 +14,7 @@ namespace DataAccessLayer.Concrete
         {
 
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Server=78.135.105.238;Database=u7174244_ProBudy;User Id=u7174244_ProBudy; Password=KYyp18Q1HPaj97N;");
-        }
+        
         //public Context(DbContextOptions<Context> options) : base(options)+
         //{
 
