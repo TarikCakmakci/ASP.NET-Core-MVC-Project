53488ad6dd86fd068e432bdd18fab13bb21e8f75
Tarık Çakmakcı <tarikcakmakci@outlook.com>
2022-06-11 15:35:36 +0300

    Update
    Updated swagger and get list api
    Connecting the blog api project to the database is done

---
+++ ASP.NET-Core-MVC-Project/BlogApi/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using BlogApi.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult EmployeeList()
+        {
+            using var c= new Context();
+            var values = c.Employees.ToList();
+            return Ok(values);
+
+        }
+    }
+}
--- ASP.NET-Core-MVC-Project/BlogApi/DataAccessLayer/Context.cs
+++ ASP.NET-Core-MVC-Project/BlogApi/DataAccessLayer/Context.cs
@@ -10,7 +10,8 @@ namespace BlogApi.DataAccessLayer
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=78.135.105.238;Database=u7174244_ProBudy;User Id=u7174244_ProBudy; Password=KYyp18Q1HPaj97N;");
+            optionsBuilder.UseSqlServer(@"Server=78.135.105.238;Database=u7174244_Pro;User Id=u7174244_Pro; Password=AE7A70C1-13DB-4D9E-9C1E-07D0856700DE;");
         }
+        public DbSet <Employee> Employees { get; set; }
     }
 }
